9cccd744099d31aed57df35411e28bb2b4b326a7
Nery Joseph Melgar Canelo <nmelgar-@student.42madrid.com>
2023-05-07 04:16:35 +0200

    mucho, mvp

---
+++ script.js
@@ -0,0 +1,28 @@
+function showDiv1(){
+	document.getElementById('Div1').style.display = '';
+	document.getElementById('Div2').style.display = 'none';
+	document.getElementById('Div3').style.display = 'none';
+	document.getElementById('Div4').style.display = 'none';
+}
+
+function showDiv2(){
+	document.getElementById('Div1').style.display = 'none';
+	document.getElementById('Div2').style.display = '';
+	document.getElementById('Div3').style.display = 'none';
+	document.getElementById('Div4').style.display = 'none';
+}
+function showDiv3(){
+	document.getElementById('Div1').style.display = 'none';
+	document.getElementById('Div2').style.display = 'none';
+	document.getElementById('Div3').style.display = '';
+	document.getElementById('Div4').style.display = 'none';
+}
+function showDiv4(){
+	document.getElementById('Div1').style.display = 'none';
+	document.getElementById('Div2').style.display = 'none';
+	document.getElementById('Div3').style.display = 'none';
+	document.getElementById('Div4').style.display = '';
+}
+function openPage(url) {
+	window.open(url, '_blank'); // Abre la URL en una nueva pestaña del navegador
+}
